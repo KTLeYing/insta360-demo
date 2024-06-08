8be9705e7c59eca70ddc87e6e1405641556450d4
mazhenle <2198902814@qq.com>
2024-06-08 23:26:31 +0800

    feat: 抢X3定时任务脚本

---
+++ src/main/java/com/mzl/insta360demo/entity/User.java
@@ -0,0 +1,41 @@
+package com.mzl.insta360demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 用户表
+ * </p>
+ *
+ * @author mzl
+ * @since 2023-12-13
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String phone;
+
+    private String address;
+
+    private String password;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
---
+++ src/main/java/com/mzl/insta360demo/infrastructure/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.mzl.insta360demo.infrastructure.mapper;
+
+import com.mzl.insta360demo.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author mzl
+ * @since 2023-12-13
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
