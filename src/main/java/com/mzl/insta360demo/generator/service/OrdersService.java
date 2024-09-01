12f84ed8c8d1ce49002756617a8720bcd7007ad4
mazhenle <2198902814@qq.com>
2024-09-02 00:47:01 +0800

    feat: 分布式限流，固定窗口计数法

---
+++ src/main/java/com/mzl/insta360demo/generator/service/OrdersService.java
@@ -0,0 +1,16 @@
+package com.mzl.insta360demo.generator.service;
+
+import com.mzl.insta360demo.generator.entity.Orders;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 订单表 服务类
+ * </p>
+ *
+ * @author mzl
+ * @since 2024-07-28
+ */
+public interface OrdersService extends IService<Orders> {
+
+}
